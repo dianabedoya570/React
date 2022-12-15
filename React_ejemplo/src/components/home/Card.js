de311c97edc5a52b76236afb1a2c822c0644c339
DianaBedoya <dianabedoya570@gmail.com>
2022-12-14 19:36:44 -0500

    Se configuran los estilos de la carta

--- React_ejemplo/src/components/home/Card.js
+++ React_ejemplo/src/components/home/Card.js
@@ -1,16 +1,16 @@
 import React from "react";
+import "../../assets/css/card.css";
 
-export default function Card(){
-    return(
-        <div>
-            <img id="card__img" src="https://es.web.img3.acsta.net/pictures/18/10/31/17/34/2348073.jpg"/>
-            <p id="card__id" >nombre</p>
-            <p id="card_lastLocation">Last Location </p>
-            <p id="card__location">ubicacion Aqui </p>
-        </div>
-
-    )
-    
-
-
+export default function Card() {
+  return (
+    <div className="card">
+      <img
+        id="card__img"
+        src="https://es.web.img3.acsta.net/pictures/18/10/31/17/34/2348073.jpg"
+      />
+      <p id="card__name">nombre</p>
+      <p id="card_lastLocation">Last Location </p>
+      <p id="card__location">ubicacion Aqui </p>
+    </div>
+  );
 }
